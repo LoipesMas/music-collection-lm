07d0e8a8d0427d28b8c04f9bc481daa35ae46118
LoipesMas <46327403+LoipesMas@users.noreply.github.com>
2021-10-02 11:14:54 +0200

    Fix static url

--- musiccollection/settings.py
+++ musiccollection/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATIC_URL = "/static/"
+STATIC_URL = "/music/static/"
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"),)
 
